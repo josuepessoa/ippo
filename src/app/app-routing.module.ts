d21c232edf6cc7dc7f123072d7dbfabe3810a002
Josue Matheus Pessoa <josue.matheus12@gmail.com>
2021-12-22 12:23:03 -0300

    commit inicial para deploy de teste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
+import { ContatoComponent } from './contato/contato.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent,
+  },
+  {
+    path: 'projetos',
+    component: ProjetosComponent,
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
